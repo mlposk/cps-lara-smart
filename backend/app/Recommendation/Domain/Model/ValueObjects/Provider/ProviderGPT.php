2c2176eeb976e0d445003224e97bfa22263849c7
a. poluyan <a.poluyan@gazpromcps.ru>
2024-09-22 14:42:34 +0300

    feat: added validation in provider

--- backend/app/Recommendation/Domain/Model/ValueObjects/Provider/ProviderGPT.php
+++ backend/app/Recommendation/Domain/Model/ValueObjects/Provider/ProviderGPT.php
@@ -4,6 +4,7 @@ namespace App\Recommendation\Domain\Model\ValueObjects\Provider;
 
 use App\Recommendation\Domain\Contracts\ValueObjects\Provider\RecommendationProviderInterface;
 use App\Recommendation\Domain\Model\ValueObjects\Provider\Result;
+use Exception;
 
 class ProviderGPT implements RecommendationProviderInterface
 {
@@ -32,16 +33,28 @@ class ProviderGPT implements RecommendationProviderInterface
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function getSuggestion($query): Result
     {
         $response_data = $this->getResult($query);
+
+        if (!isset($response_data['choices'][0]['message']['content'])) {
+            throw new Exception('Invalid response format: content is missing');
+        }
+
         $response = explode('%d%', $response_data['choices'][0]['message']['content']);
+
+        if (count($response) < 2) {
+            throw new Exception('Invalid response format: expected 2 parts, but received ' . count($response));
+        }
 
         return new Result(new SmartTitle($response[0]), new Recommendation($response[1]));
     }
 
+    /**
+     * @throws Exception
+     */
     private function getResult($query): array
     {
         $ch = curl_init($this->apiUrl);
@@ -51,7 +64,16 @@ class ProviderGPT implements RecommendationProviderInterface
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($query));
 
         $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
+
+        if ($response === false) {
+            throw new Exception('Failed to get response from language model');
+        }
+
+        if ($httpCode !== 200) {
+            throw new Exception("Language model returned an invalid status code: $httpCode");
+        }
 
         return json_decode($response, true);
     }
