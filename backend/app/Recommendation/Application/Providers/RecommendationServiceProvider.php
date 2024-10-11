cb3ffb4fb6eea4a5c7e604702ee37ac75422a700
Alexander Molokotin <a.molokotin@gazpromcps.ru>
2024-10-11 12:12:43 +0500

    #73530 hotfix(expert): move into config

--- backend/app/Recommendation/Application/Providers/RecommendationServiceProvider.php
+++ backend/app/Recommendation/Application/Providers/RecommendationServiceProvider.php
@@ -6,8 +6,19 @@ use Illuminate\Support\ServiceProvider;
 
 class RecommendationServiceProvider extends ServiceProvider
 {
+    public array $providers = [
+        "gpt" => \App\Recommendation\Domain\Model\ValueObjects\Expert\ExpertGPT::class,
+        "gigachat" => \App\Recommendation\Domain\Model\ValueObjects\Expert\ExpertGigaChat::class
+    ];
+
+    public array $experts = [
+        "gpt" => \App\Recommendation\Domain\Model\ValueObjects\Provider\ProviderGPT::class,
+        "gigachat" => \App\Recommendation\Domain\Model\ValueObjects\Provider\ProviderGigaChat::class,
+    ];
+
     public function register()
     {
+        $model = config('expert.model');
         $this->app->bind(
             \App\Recommendation\Domain\Contracts\Repositories\RecommendationRepositoryInterface::class,
             \App\Recommendation\Application\Repositories\Eloquent\RecommendationRepository::class,
@@ -15,14 +26,12 @@ class RecommendationServiceProvider extends ServiceProvider
 
         $this->app->bind(
             \App\Recommendation\Domain\Contracts\ValueObjects\Expert\RecommendationExpertInterface::class,
-//            \App\Recommendation\Domain\Model\ValueObjects\Expert\ExpertGPT::class,
-            \App\Recommendation\Domain\Model\ValueObjects\Expert\ExpertGigaChat::class,
+            $this->experts[$model],
         );
 
         $this->app->bind(
             \App\Recommendation\Domain\Contracts\ValueObjects\Provider\RecommendationProviderInterface::class,
-//            \App\Recommendation\Domain\Model\ValueObjects\Provider\ProviderGPT::class,
-            \App\Recommendation\Domain\Model\ValueObjects\Provider\ProviderGigaChat::class,
+            $this->providers[$model],
         );
     }
 }
---
+++ backend/config/expert.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    "model" => env("EXPERT_MODEL", "gpt"),
+];
